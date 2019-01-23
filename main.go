e5b224df09bf63e9f5517ba061f065fa64ff402e
Samet Albayrak <MuhammetSamet.Albayrak@intertech.com.tr>
2019-01-23 14:55:53 +0300

    first commit

---
+++ main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"fmt"
+	"os"
+)
+
+func main() {
+
+	//Getenv examples
+	uName := os.Getenv("USER")
+	uDomain := os.Getenv("USERDOMAIN")
+	processorArchitecture := os.Getenv("PROCESSOR_ARCHITECTURE")
+	goRootPAth := os.Getenv("GOROOT")
+	goPath := os.Getenv("GOPATH")
+	homePath := os.Getenv("HOMEPATH")
+
+	fmt.Println(uName)
+	fmt.Println(uDomain)
+	fmt.Println(processorArchitecture)
+	fmt.Println(goRootPAth)
+	fmt.Println(goPath)
+	fmt.Println(homePath)
+}
